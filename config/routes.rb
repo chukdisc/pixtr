44c09fa3487a553a3e163e58970edd2213523d8c
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-17 12:01:43 -0400

    Users can add an image to a gallery

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "galleries#index"
-  resources :galleries, only: [:show, :new, :create, :edit, :update, :destroy]
+
+  resources :galleries, only: [:show, :new, :create, :edit, :update, :destroy] do
+    get "/images/new" => "images#new"
+    post "/images" => "images#create"
+  end
 end
