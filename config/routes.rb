7937e6bf3e329fd7b283ea2338a849a942b39258
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-06-24 10:33:20 -0400

    Add groups

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :galleries, only: [:show, :new, :create, :edit, :update, :destroy] do
     resources :images, only: [:new, :create, :edit, :update]
   end
+  resources :groups, only: [:new, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619200601) do
+ActiveRecord::Schema.define(version: 20140624140406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20140619200601) do
   end
 
   add_index "galleries", ["user_id"], name: "index_galleries_on_user_id", using: :btree
+
+  create_table "groups", force: true do |t|
+    t.string "name",        null: false
+    t.text   "description", null: false
+  end
+
+  add_index "groups", ["name"], name: "index_groups_on_name", unique: true, using: :btree
 
   create_table "images", force: true do |t|
     t.integer  "gallery_id"
